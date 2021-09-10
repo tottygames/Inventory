08d03510d58b5ccd4e69f089340b6badf1826dd6
tottygames <88378224+tottygames@users.noreply.github.com>
2021-09-10 19:31:29 +0300

    UI inventory

---
+++ UIInventory/Assets/PlayScene/SkillAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillAnimation : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void ButtonClick(int num)
+    {
+        switch (num)
+        {
+            case 1:
+                anim.SetBool("Cold", true);
+                break;
+            case 2:
+                anim.SetBool("Fire", true);
+                break;
+            case 3:
+                anim.SetBool("Dark", true);
+                break;
+        }
+    }
+}
